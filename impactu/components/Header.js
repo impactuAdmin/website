d0e1940b4f90cf01a7d3fc64097a84b0db5e8ab2
Duarte Carvalho <duarte.carvalho@g.xarevision.pt>
2021-11-06 15:42:23 +0000

    fixed bar link and next not found issue

--- impactu/components/Header.js
+++ impactu/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div><nav className='navigation_wrapper'>
         <div className='navigation'>
-            <Link href="/Home"><a className='logo-container' href='#'>
+            <Link href="/"><a className='logo-container' href='#'>
                 <svg className='logo desktop' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 686 161" fill="none">
                     <path d="M31.6571 25.8997H0.748047V122.235H31.6571V25.8997Z" fill="#ffffff" />
                     <path fillRule="evenodd" clipRule="evenodd"
