cd64bc41e8e14a8fd40d659cba8e91e7403b7cb1
Duarte Carvalho <duarte.carvalho@g.xarevision.pt>
2021-12-25 17:13:52 +0000

    replaced video with gif

--- impactu/components/Hero.js
+++ impactu/components/Hero.js
@@ -1,9 +1,10 @@
 const Hero = () => {
     return (
         <section className="hero-wrapper">
-            <video id="video" width='100%' height='100%' autoPlay loop muted playsInline>
+            {/* <video id="video" width='100%' height='100%' autoPlay loop muted playsInline>
                 <source src='../video_hero.mp4' type="video/mp4" />
-            </video>
+            </video> */}
+            {/* <img src="/videoGif.gif" alt="This is an animated gif image, but it does not move" width='100%' height='100%'/> */}
         <div className='title-hero'>
             <div className='animate-title'>
                 <h1>O impacto social começa em ti.</h1>
