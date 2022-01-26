c9e9262881bab5751d9863aeb4d6a6255e16feaf
afonsogmarques <afonsogmarques@gmail.com>
2022-01-26 16:53:03 +0000

    buéda alterações cool

--- impactu/pages/Contactos.js
+++ impactu/pages/Contactos.js
@@ -114,7 +114,21 @@ const Contactos = () => {
                                 <label htmlFor="subject">Assunto/Mensagem<span className="asterisk"> *</span></label>
                                 <textarea id="subject" name="subject" placeholder="Escreve a tua mensagem" required style={{ heigth: "200px" }} />
 
-                                <input type="submit" value="Enviar" />
+                                <input type="submit" value="Enviar" className="hidden-button"/>
+                                <div className='link intro contactos'>
+                                    <div className='link-underline contactos intro'>
+                                        <a>
+                                            <h4>Enviar</h4>
+                                            <svg className='arrow' xmlns="http://www.w3.org/2000/svg" width="19" height="14" viewBox="0 0 19 14">
+                                                <path d="M17.6151 7.0705H0.515381V7.99454H17.6151V7.0705Z" strokeWidth="1"
+                                                    strokeMiterlimit="102" />
+                                                <path d="M18.5153 7.30319L12.1274 1L11.5484 1.57138L17.9363 7.87457L18.5153 7.30319Z" strokeWidth="1" strokeMiterlimit="10" />
+                                                <path d="M17.3516 7.30151L11.5464 13.0297L12.1254 13.6011L17.9306 7.8729L17.3516 7.30151Z" strokeWidth="1" strokeMiterlimit="10" />
+                                            </svg>
+                                        </a>
+                                    </div>
+                                 </div>
+                                
                             </form>
                         </div>
                     </div>
