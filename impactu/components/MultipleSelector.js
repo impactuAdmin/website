1244a67ea564e0165e0d4f6d55056e20c3e328b7
Duarte Carvalho <duarte.carvalho@g.xarevision.pt>
2022-09-24 12:34:08 +0100

    Added required field

--- impactu/components/MultipleSelector.js
+++ impactu/components/MultipleSelector.js
@@ -16,7 +16,7 @@ const MultipleSelector = ( { dates } ) => {
     }
 
     return (
-        <select id="dataEscolhida" name="Data preferencia" multiple size="4" value={selectedDates} onChange={(e) => handleChange(e)}>
+        <select id="dataEscolhida" name="Data preferencia" multiple size="4" value={selectedDates} onChange={(e) => handleChange(e)} required>
             {dates.map((date) =>
                 <option key={date.id} id={date.id} value={date.data} >{date.data}</option>
             )}
