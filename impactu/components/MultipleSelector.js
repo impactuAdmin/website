ce9c989c424c2fdaa23bd997f2f8395477508215
Duarte Carvalho <duarte.carvalho@g.xarevision.pt>
2022-09-24 16:55:04 +0100

    React selector component

--- impactu/components/MultipleSelector.js
+++ impactu/components/MultipleSelector.js
@@ -1,26 +1,23 @@
 import { useState } from 'react';
+import Select from "react-select";
 
-const MultipleSelector = ( { dates } ) => {
+const MultipleSelector = ({ dates }) => {
 
-    const [selectedDates, selectedDatesSet] = useState([]);
+    const [selectedDates, selectedDatesSet] = useState();
 
-    const handleChange = (e) => {
-        const clickedDate = e.target.value
-
-        if(selectedDates.includes(clickedDate)) { // if already includes, delete
-            selectedDatesSet(selectedDates.filter(date => date !== clickedDate))
-        }
-        else { // otherwise, add
-            selectedDatesSet([...selectedDates, clickedDate]) 
-        }
+    const handleChange = (data) => {
+        selectedDatesSet(data)
     }
 
     return (
-        <select id="dataEscolhida" name="Data preferencia" multiple size="4" value={selectedDates} onChange={(e) => handleChange(e)} required>
-            {dates.map((date) =>
-                <option key={date.id} id={date.id} value={date.data} selected={selectedDates.includes(date)}>{date.data}</option>
-            )}
-        </select>
+        <Select
+            id="dataEscolhida"
+            name="Data preferencia"
+            placeholder="Seleciona as tuas preferências"
+            options={dates}
+            value={selectedDates}
+            onChange={handleChange}
+            isMulti />
     )
 }
 
--- impactu/public/data/dados.js
+++ impactu/public/data/dados.js
@@ -1,89 +1,89 @@
 export const datas = [
-    {
-        id: 1,
-        data: "7 de outubro (manhã) - Lisboa"
-    },
-    {
-        id: 2,
-        data: "7 de outubro (tarde) - Lisboa"
-    },
-    {
-        id: 3,
-        data: "8 de outubro (manhã) - Lisboa"
-    },
-    {
-        id: 4,
-        data: "8 de outubro (tarde) - Lisboa"
-    },
-    {
-        id: 5,
-        data: "10 de outubro (manhã) - Porto"
-    },
-    {
-        id: 6,
-        data: "10 de outubro (tarde) - Porto"
-    },
-    {
-        id: 7,
-        data: "11 de outubro (manhã) - Porto"
-    },
-    {
-        id: 8,
-        data: "11 de outubro (tarde) - Porto"
-    },
-    {
-        id: 9,
-        data: "12 de outubro (manhã) - Porto"
-    }
+  {
+    label: "7 de outubro (manhã) - Lisboa",
+    value: "7 de outubro (manhã) - Lisboa"
+  },
+  {
+    label: "7 de outubro (tarde) - Lisboa",
+    value: "7 de outubro (tarde) - Lisboa"
+  },
+  {
+    label: "8 de outubro (manhã) - Lisboa",
+    value: "8 de outubro (manhã) - Lisboa"
+  },
+  {
+    label: "8 de outubro (tarde) - Lisboa",
+    value: "8 de outubro (tarde) - Lisboa"
+  },
+  {
+    label: "10 de outubro (manhã) - Porto",
+    value: "10 de outubro (manhã) - Porto"
+  },
+  {
+    label: "10 de outubro (tarde) - Porto",
+    value: "10 de outubro (tarde) - Porto"
+  },
+  {
+    label: "11 de outubro (manhã) - Porto",
+    value: "11 de outubro (manhã) - Porto"
+  },
+  {
+    label: "11 de outubro (tarde) - Porto",
+    value: "11 de outubro (tarde) - Porto"
+  },
+  {
+    label: "12 de outubro (manhã) - Porto",
+    value: "12 de outubro (manhã) - Porto"
+  },
 ];
 
 export const pdfs = [
-    {
-        linkParam: "Estatutos",
-        title: "Estatutos",
-        file: "NovosEstatutosdaAssociaçãoImpactu.pdf"
-    },
-    {
-        linkParam: "PlanoDeAtividades",
-        title: "Plano de Atividades",
-        file: "PlanoDeAtividades.pdf"
-    },
-    {
-        linkParam: "RelatorioContas",
-        title: "Relatório de Contas 2021",
-        file: "RelatorioContas2020.pdf"
-    },
-    {
-        linkParam: "PoliticaPrivacidade",
-        title: "Política de Privacidade",
-        file: "politicaDePrivacidade.pdf"
-    }
+  {
+    linkParam: "Estatutos",
+    title: "Estatutos",
+    file: "NovosEstatutosdaAssociaçãoImpactu.pdf",
+  },
+  {
+    linkParam: "PlanoDeAtividades",
+    title: "Plano de Atividades",
+    file: "PlanoDeAtividades.pdf",
+  },
+  {
+    linkParam: "RelatorioContas",
+    title: "Relatório de Contas 2021",
+    file: "RelatorioContas2020.pdf",
+  },
+  {
+    linkParam: "PoliticaPrivacidade",
+    title: "Política de Privacidade",
+    file: "politicaDePrivacidade.pdf",
+  },
 ];
 
 export const departamentos = [
-    {
-        id: 1,
-        name: "Departamento Social (intervenção social)",
-        value: "Social"
-    },
-    {
-        id: 2,
-        name: "Departamento de Projetos",
-        value: "DepartamentoDeProjetos"
-    },
-    {
-        id: 3,
-        name: "Departamento Comunicação e Marketing",
-        value: "Marketing"
-    },
-    {
-        id: 4,
-        name: "Departamento Relações Externas",
-        value: "Relacoesexternas"
-    },
-    {
-        id: 5,
-        name: "Departamento de Recursos Humanos",
-        value: "Recursoshumanos"
-    },
+  {
+    id: 1,
+    name: "Departamento Social (intervenção social)",
+    value: "Social",
+  },
+  {
+    id: 2,
+    name: "Departamento de Projetos",
+    value: "DepartamentoDeProjetos",
+  },
+  {
+    id: 3,
+    name: "Departamento Comunicação e Marketing",
+    value: "Marketing",
+  },
+  {
+    id: 4,
+    name: "Departamento Relações Externas",
+    value: "Relacoesexternas",
+  },
+  {
+    id: 5,
+    name: "Departamento de Recursos Humanos",
+    value: "Recursoshumanos",
+  },
 ];
