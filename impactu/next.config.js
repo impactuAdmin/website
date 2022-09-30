dff8b0e2dba2cdee719cf0b425c84227e19f9138
Duarte Carvalho <duarte.carvalho@g.xarevision.pt>
2022-09-30 15:41:21 +0100

    removed redirects from nextjs config

--- impactu/next.config.js
+++ impactu/next.config.js
@@ -3,14 +3,5 @@ module.exports = {
   distDir: '.next',
   images: {
     deviceSizes: [480, 768, 1080],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/Recrutamento',
-        destination: '/',
-        permanent: true,
-      },
-    ]
-  },
+  }
 }
