24734d254d250274c06681bd5a72ca52cc986535
Duarte Carvalho <duarte.carvalho@g.xarevision.pt>
2022-11-17 09:41:25 +0000

    Added redirect rule

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ module.exports = {
   distDir: 'build',
   images: {
     deviceSizes: [480, 768, 1080],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/recrutamento',
+        destination: '/',
+        permanent: true,
+      },
+    ]
   }
 }
