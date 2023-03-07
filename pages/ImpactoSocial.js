45bb3502ec2e50c9ba4bc015fcdd6383cbddd4d8
rodrigues-tiago <tiago.rodrigues1002@gmail.com>
2023-03-07 13:02:49 +0000

    Add Recrutamento and fix content

--- pages/ImpactoSocial.js
+++ pages/ImpactoSocial.js
@@ -2,53 +2,57 @@ import Departments from '../components/Departments'
 import Projects from '../components/Projects'
 
 const ImpactoSocial = () => {
-    return (
-        <>
-            <section className="impactosocial-hero-wrapper">
-                <div className="impactosocial-hero-content">
-                    <div className='impactosocial-title'>
-                        <h1>Impacto social</h1>
-                    </div>
-                </div>
-            </section>
-            <section className="impactosocial-background">
-                <section className="impactosocial-intro-content reveal">
-                    <div className="impactosocial-intro-title">
-                        <h1>O nosso impacto:</h1>
-                        <h2>Social</h2>
-                    </div>
-                    <div className="impactosocial-intro-title-2">
-                        <div className="impactosocial-intro-img">
-                            <img src="/senhora.webp" alt="senhora" />
-                        </div>
+  return (
+    <>
+      <section className="impactosocial-hero-wrapper">
+        <div className="impactosocial-hero-content">
+          <div className="impactosocial-title">
+            <h1>Impacto social</h1>
+          </div>
+        </div>
+      </section>
+      <section className="impactosocial-background">
+        <section className="impactosocial-intro-content reveal">
+          <div className="impactosocial-intro-title">
+            <h1>O nosso impacto:</h1>
+            <h2>Social</h2>
+          </div>
+          <div className="impactosocial-intro-title-2">
+            <div className="impactosocial-intro-img">
+              <img src="/senhora.webp" alt="senhora" />
+            </div>
 
-                        <div className="impactosocial-intro-text">
-                            <h3>A Impac&rsquo;tu é uma associação de Impacto Social, composta por estudantes universitários
-                                das Academias do Porto e Lisboa, com a missão de apoiar famílias em situação
-                                socioeconómica mais debilitada.
-                                <br />
-                                <br />
-                                Enquadrada dentro dos ODS <br />
-                                1 - Erradicar a Pobreza;<br />
-                                4 - Educação de Qualidade;<br />
-                                10 - Reduzir as desigualdades;<br />
-                                11 - Cidades e Comunidades Sustentáveis, a Associação trabalha na resiliência
-                                e capacitação das famílias como na redução do nível de pobreza. Guiados pelos
-                                objetivo de Inclusão social acreditamos que só conseguiremos criar impacto garantindo
-                                a igualdade de oportunidades para todos e redução de desigualdades, sem esquecer que
-                                uma dessas garantias será o acesso à habitação digna e serviços básicos.<br />
-                                <br />
-
-                                Vamos criar impacto?
-                            </h3>
-                        </div>
-                    </div>
-                </section>
-                <Projects />
-                <Departments />
-            </section>
-        </>
-    )
+            <div className="impactosocial-intro-text">
+              <h3>
+                A Impac&rsquo;tu é uma associação de Impacto Social, composta por estudantes
+                universitários das Academias do Porto e Lisboa, com a missão de apoiar famílias em
+                situação socioeconómica mais debilitada.
+                <br />
+                <br />
+                Enquadrada dentro dos ODS <br />
+                1 - Erradicar a Pobreza;
+                <br />
+                4 - Educação de Qualidade;
+                <br />
+                10 - Reduzir as desigualdades;
+                <br />
+                11 - Cidades e Comunidades Sustentáveis, a Associação trabalha na resiliência e
+                capacitação das famílias como na redução do nível de pobreza. Guiados pelos objetivo
+                de Inclusão social acreditamos que só conseguiremos criar impacto garantindo a
+                igualdade de oportunidades para todos e redução de desigualdades, sem esquecer que
+                uma dessas garantias será o acesso à habitação digna e serviços básicos.
+                <br />
+                <br />
+                Vamos criar impacto?
+              </h3>
+            </div>
+          </div>
+        </section>
+        <Projects />
+        {/* <Departments /> */}
+      </section>
+    </>
+  )
 }
 
-export default ImpactoSocial;
+export default ImpactoSocial
--- public/data/dados.js
+++ public/data/dados.js
@@ -1,89 +1,89 @@
 export const datas = [
-  // {
-  //   label: "7 de outubro (manhã) - Lisboa",
-  //   value: "7 de outubro (manhã) - Lisboa"
-  // },
-  // {
-  //   label: "7 de outubro (tarde) - Lisboa",
-  //   value: "7 de outubro (tarde) - Lisboa"
-  // },
-  // {
-  //   label: "8 de outubro (manhã) - Lisboa",
-  //   value: "8 de outubro (manhã) - Lisboa"
-  // },
-  // {
-  //   label: "8 de outubro (tarde) - Lisboa",
-  //   value: "8 de outubro (tarde) - Lisboa"
-  // },
-  // {
-  //   label: "10 de outubro (manhã) - Porto",
-  //   value: "10 de outubro (manhã) - Porto"
-  // },
-  // {
-  //   label: "10 de outubro (tarde) - Porto",
-  //   value: "10 de outubro (tarde) - Porto"
-  // },
-  // {
-  //   label: "11 de outubro (manhã) - Porto",
-  //   value: "11 de outubro (manhã) - Porto"
-  // },
-  // {
-  //   label: "11 de outubro (tarde) - Porto",
-  //   value: "11 de outubro (tarde) - Porto"
-  // },
-  // {
-  //   label: "12 de outubro (manhã) - Porto",
-  //   value: "12 de outubro (manhã) - Porto"
-  // },
-];
+  {
+    label: '7 de outubro (manhã) - Lisboa',
+    value: '7 de outubro (manhã) - Lisboa',
+  },
+  {
+    label: '7 de outubro (tarde) - Lisboa',
+    value: '7 de outubro (tarde) - Lisboa',
+  },
+  {
+    label: '8 de outubro (manhã) - Lisboa',
+    value: '8 de outubro (manhã) - Lisboa',
+  },
+  {
+    label: '8 de outubro (tarde) - Lisboa',
+    value: '8 de outubro (tarde) - Lisboa',
+  },
+  {
+    label: '10 de outubro (manhã) - Porto',
+    value: '10 de outubro (manhã) - Porto',
+  },
+  {
+    label: '10 de outubro (tarde) - Porto',
+    value: '10 de outubro (tarde) - Porto',
+  },
+  {
+    label: '11 de outubro (manhã) - Porto',
+    value: '11 de outubro (manhã) - Porto',
+  },
+  {
+    label: '11 de outubro (tarde) - Porto',
+    value: '11 de outubro (tarde) - Porto',
+  },
+  {
+    label: '12 de outubro (manhã) - Porto',
+    value: '12 de outubro (manhã) - Porto',
+  },
+]
 
 export const pdfs = [
   {
-    linkParam: "Estatutos",
-    title: "Estatutos",
-    file: "NovosEstatutosdaAssociaçãoImpactu.pdf",
+    linkParam: 'Estatutos',
+    title: 'Estatutos',
+    file: 'NovosEstatutosdaAssociaçãoImpactu.pdf',
   },
   {
-    linkParam: "PlanoDeAtividades",
-    title: "Plano de Atividades",
-    file: "PlanoDeAtividades.pdf",
+    linkParam: 'PlanoDeAtividades',
+    title: 'Plano de Atividades',
+    file: 'PlanoDeAtividades.pdf',
   },
   {
-    linkParam: "RelatorioContas",
-    title: "Relatório de Contas 2021",
-    file: "RelatorioContas2020.pdf",
+    linkParam: 'RelatorioContas',
+    title: 'Relatório de Contas 2021',
+    file: 'RelatorioContas2020.pdf',
   },
   {
-    linkParam: "PoliticaPrivacidade",
-    title: "Política de Privacidade",
-    file: "politicaDePrivacidade.pdf",
+    linkParam: 'PoliticaPrivacidade',
+    title: 'Política de Privacidade',
+    file: 'politicaDePrivacidade.pdf',
   },
-];
+]
 
 export const departamentos = [
   {
     id: 1,
-    name: "Departamento Social (intervenção social)",
-    value: "Social",
+    name: 'Departamento Social (intervenção social)',
+    value: 'Social',
   },
   {
     id: 2,
-    name: "Departamento de Projetos",
-    value: "DepartamentoDeProjetos",
+    name: 'Departamento de Projetos',
+    value: 'DepartamentoDeProjetos',
   },
   {
     id: 3,
-    name: "Departamento Comunicação e Marketing",
-    value: "Marketing",
+    name: 'Departamento Comunicação e Marketing',
+    value: 'Marketing',
   },
   {
     id: 4,
-    name: "Departamento Relações Externas",
-    value: "Relacoesexternas",
+    name: 'Departamento Relações Externas',
+    value: 'Relacoesexternas',
   },
   {
     id: 5,
-    name: "Departamento de Recursos Humanos",
-    value: "Recursoshumanos",
+    name: 'Departamento de Recursos Humanos',
+    value: 'Recursoshumanos',
   },
-];
+]
