e4df57e25ca4e79256bd3a49f024b3eca0d17636
rodrigues-tiago <tiago.rodrigues1002@gmail.com>
2023-09-03 23:27:01 +0100

    Recrutamento live

--- public/data/dados.js
+++ public/data/dados.js
@@ -97,53 +97,53 @@ export const departamentos = [
 ]
 
 export const merchProducts = [
-  {
-    linkParam: 'Boné',
-    name: 'Boné Impac’tu',
-    price: { value: 13.5, display: '13,50' },
-    images: ['/bone.webp', '/bone2.webp'],
-    description:
-      'Com o nosso novo boné vais-nos ajudar a elevar mais histórias e ainda vais elevar o teu look! ',
-    characteristics: 'Boné verde vintage 6 painéis, 100% algodão, com logotipo bordado na frente.',
-  },
-  {
-    linkParam: 'TshirtBordada',
-    name: 'T-Shirt Bordada',
-    price: { value: 12, display: '12,00' },
-    images: ['/tshirt.webp', '/tshirt2.webp'],
-    description: 'Cria mais impacto na sociedade com a nossa t-shirt bordada.',
-    characteristics:
-      'T-Shirt unissexo branca, 100% algodão. Com a frase “vamos criar impacto?” bordada no lado esquerdo do peito.',
-    sizes: ['S', 'M', 'L'],
-  },
-  {
-    linkParam: 'TshirtImpactador',
-    name: 'T-shirt Impactador',
-    price: { value: 12, display: '12,00' },
-    images: ['/bone.webp', '/bone.webp'],
-    // colors: ['verde', 'cinzento'],
-    description: 'Vais-te sentir um impactador com a nossa t-shirt original.',
-    characteristics:
-      'T-Shirt unissexo verde, 100% algodão. Com impressão frente e costas a branco.',
-    sizes: ['S', 'M', 'L'],
-  },
-  {
-    linkParam: 'SweatshirtImpactador',
-    name: 'Sweatshirt impactador',
-    price: { value: 25, display: '25,00' },
-    images: ['/sweat.webp', '/sweat2.webp', '/sweat3.webp'],
-    description: 'A nossa sweatshirt original é a peça necessária em qualquer roupeiro.',
-    characteristics:
-      'Sweatshirt verde com capuz unissexo. Manga comprida com punhos em malha canelada. Interior com felpa. Cós em malha canelada. Com impressão frente e costas a branco.',
-    sizes: ['S', 'M', 'L'],
-  },
-  {
-    linkParam: 'ToteBag',
-    name: 'Tote Bag Impac’tu',
-    price: { value: 7.5, display: '7,50' },
-    images: ['/tote.webp'],
-    description:
-      'A bolsa que todos precisam! A forma perfeita de nos ajudares a elevar mais histórias, com impacto no teu dia-a-dia. Depois de a teres, nunca mais vais querer largar!',
-    characteristics: 'Tote bag, com impressão do logotipo a verde. Com bolso no interior.',
-  },
+  //   {
+  //     linkParam: 'Boné',
+  //     name: 'Boné Impac’tu',
+  //     price: { value: 13.5, display: '13,50' },
+  //     images: ['/bone.webp', '/bone2.webp'],
+  //     description:
+  //       'Com o nosso novo boné vais-nos ajudar a elevar mais histórias e ainda vais elevar o teu look! ',
+  //     characteristics: 'Boné verde vintage 6 painéis, 100% algodão, com logotipo bordado na frente.',
+  //   },
+  //   {
+  //     linkParam: 'TshirtBordada',
+  //     name: 'T-Shirt Bordada',
+  //     price: { value: 12, display: '12,00' },
+  //     images: ['/tshirt.webp', '/tshirt2.webp'],
+  //     description: 'Cria mais impacto na sociedade com a nossa t-shirt bordada.',
+  //     characteristics:
+  //       'T-Shirt unissexo branca, 100% algodão. Com a frase “vamos criar impacto?” bordada no lado esquerdo do peito.',
+  //     sizes: ['S', 'M', 'L'],
+  //   },
+  //   {
+  //     linkParam: 'TshirtImpactador',
+  //     name: 'T-shirt Impactador',
+  //     price: { value: 12, display: '12,00' },
+  //     images: ['/bone.webp', '/bone.webp'],
+  //     // colors: ['verde', 'cinzento'],
+  //     description: 'Vais-te sentir um impactador com a nossa t-shirt original.',
+  //     characteristics:
+  //       'T-Shirt unissexo verde, 100% algodão. Com impressão frente e costas a branco.',
+  //     sizes: ['S', 'M', 'L'],
+  //   },
+  //   {
+  //     linkParam: 'SweatshirtImpactador',
+  //     name: 'Sweatshirt impactador',
+  //     price: { value: 25, display: '25,00' },
+  //     images: ['/sweat.webp', '/sweat2.webp', '/sweat3.webp'],
+  //     description: 'A nossa sweatshirt original é a peça necessária em qualquer roupeiro.',
+  //     characteristics:
+  //       'Sweatshirt verde com capuz unissexo. Manga comprida com punhos em malha canelada. Interior com felpa. Cós em malha canelada. Com impressão frente e costas a branco.',
+  //     sizes: ['S', 'M', 'L'],
+  //   },
+  //   {
+  //     linkParam: 'ToteBag',
+  //     name: 'Tote Bag Impac’tu',
+  //     price: { value: 7.5, display: '7,50' },
+  //     images: ['/tote.webp'],
+  //     description:
+  //       'A bolsa que todos precisam! A forma perfeita de nos ajudares a elevar mais histórias, com impacto no teu dia-a-dia. Depois de a teres, nunca mais vais querer largar!',
+  //     characteristics: 'Tote bag, com impressão do logotipo a verde. Com bolso no interior.',
+  //   },
 ]
